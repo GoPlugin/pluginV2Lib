3258e40a0ef08877e6cdf17c502bf3d4ed59094a
raghu0891 <raghu0891@gmail.com>
2023-06-21 23:42:44 +0530

    removing pluginV2-relay

--- pluginV2-relay/pkg/monitoring/logger.go
+++
@@ -1,6 +0,0 @@
-package monitoring
-
-import "github.com/pluginV2-relay/pkg/logger"
-
-// Logger is a type alias for backwards compatibility.
-type Logger = logger.Logger
--- pluginV2-relay/script/generate.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -e
-
-cd "$(dirname "$0")"/..
-
-go generate -v ./core/services/telemetry/generated/generate.go
